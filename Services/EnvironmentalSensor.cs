c51d8e3bef0fbbcdb84e0221f46662c28c238df9
Mark Pitman <mark.pitman@gettyimages.com>
2019-09-13 13:18:25 -0700

    Brute force lock the code that accesses the sensor
    Kept getting errors about not being able to write to a pin that is not
    set for output mode. Addind a lock around the code seems to help. It's a
    little heavy-handed but it seems to help some.

--- Services/EnvironmentalSensor.cs
+++ Services/EnvironmentalSensor.cs
@@ -9,82 +9,81 @@ namespace pidashboard.Services
 {
     public class EnvironmentalSensor
     {
+        private readonly Timer _dataUpdateTimer;
         private readonly DhtBase _sensor;
-        private EnvironmentalSensorData _data;
-        private readonly Timer _dataTimer;
-        private List<EnvironmentalSensorData> _series = new List<EnvironmentalSensorData>();
+        private readonly Timer _sensorReadTimer;
+        private readonly List<EnvironmentalSensorData> _series = new List<EnvironmentalSensorData>();
+        private object _locker = new object();
 
         public EnvironmentalSensor(DhtWrapper sensor)
         {
             _sensor = sensor.DhtSensor;
-            _dataTimer = new Timer(ReadData, null, TimeSpan.Zero, TimeSpan.FromSeconds(1));
+            _sensorReadTimer = new Timer(ReadData, null, TimeSpan.Zero, TimeSpan.FromSeconds(1));
+            _dataUpdateTimer = new Timer(UpdateData, null, TimeSpan.Zero, TimeSpan.FromSeconds(10));
         }
 
-        public EnvironmentalSensorData Data => _data;
+        public EnvironmentalSensorData Data { get; private set; }
 
-        public void ReadData(object state)
+        private void UpdateData(object state)
+        {
+            if (_series.Count < 10) return;
+            var result = new EnvironmentalSensorData();
+            var standardFactor = 2.0;
+            var temperatureMean = _series.Average(x => x.Temperature.Fahrenheit);
+            var temperatureStandardDeviation =
+                Math.Sqrt(_series.Select(x => Math.Pow(x.Temperature.Fahrenheit - temperatureMean, 2)).Sum() /
+                          _series.Count);
+
+            double filteredTemperature;
+            if (Math.Abs(temperatureStandardDeviation) < 1)
+                filteredTemperature = _series.First().Temperature.Fahrenheit;
+            else
+                filteredTemperature = _series.Where(x =>
+                        x.Temperature.Fahrenheit >
+                        temperatureMean - standardFactor * temperatureStandardDeviation)
+                    .Where(x => x.Temperature.Fahrenheit <
+                                temperatureMean + standardFactor * temperatureStandardDeviation)
+                    .Average(x => x.Temperature.Fahrenheit);
+
+            result.Temperature = Temperature.FromFahrenheit(filteredTemperature);
+
+            var humidityMean = _series.Average(x => x.Humidity);
+            var humidityStandardDeviation =
+                Math.Sqrt(_series.Select(x => Math.Pow(x.Humidity - humidityMean, 2)).Sum() / _series.Count);
+
+            double filteredHumidity;
+            if (Math.Abs(humidityStandardDeviation) < 1)
+                filteredHumidity = _series.First().Humidity;
+            else
+                filteredHumidity = _series.Where(x =>
+                        x.Humidity > humidityMean - standardFactor * humidityStandardDeviation)
+                    .Where(x => x.Humidity < humidityMean + standardFactor * humidityStandardDeviation)
+                    .Average(x => x.Humidity);
+
+            result.Humidity = filteredHumidity;
+
+            Data = result;
+            _series.Clear();
+        }
+
+        private void ReadData(object state)
         {
             var result = new EnvironmentalSensorData();
             if (_sensor == null)
             {
                 result.Temperature = Temperature.FromFahrenheit(73.5);
                 result.Humidity = 60.5;
-                _data = result;
+                _series.Add(result);
             }
             else
             {
-                result.Temperature = _sensor.Temperature;
-                result.Humidity = _sensor.Humidity;
-
-                if (_sensor.IsLastReadSuccessful)
+                lock (_locker)
                 {
-                    _series.Add(result);
+                    result.Temperature = _sensor.Temperature;
+                    result.Humidity = _sensor.Humidity;
                 }
 
-                if (_series.Count >= 10)
-                {
-                    var standardFactor = 2.0;
-                    var temperatureMean = _series.Average(x => x.Temperature.Fahrenheit);
-                    var temperatureStandardDeviation = Math.Sqrt(_series.Select(x => Math.Pow((x.Temperature.Fahrenheit - temperatureMean), 2)).Sum()/_series.Count);
-
-                    double filteredTemperature;
-                    if (Math.Abs(temperatureStandardDeviation) < 1)
-                    {
-                        filteredTemperature = _series.First().Temperature.Fahrenheit;
-                    }
-                    else
-                    {
-                        filteredTemperature = _series.Where(x =>
-                                x.Temperature.Fahrenheit >
-                                (temperatureMean - standardFactor * temperatureStandardDeviation))
-                            .Where(x => x.Temperature.Fahrenheit <
-                                        temperatureMean + standardFactor * temperatureStandardDeviation)
-                            .Average(x => x.Temperature.Fahrenheit);
-                    }
-
-                    result.Temperature = Temperature.FromFahrenheit(filteredTemperature);
-                    
-                    var humidityMean = _series.Average(x => x.Humidity);
-                    var humidityStandardDeviation = Math.Sqrt(_series.Select(x => Math.Pow((x.Humidity - humidityMean), 2)).Sum()/_series.Count);
-
-                    double filteredHumidity;
-                    if (Math.Abs(humidityStandardDeviation) < 1)
-                    {
-                        filteredHumidity = _series.First().Humidity;
-                    }
-                    else
-                    {
-                        filteredHumidity = _series.Where(x =>
-                                x.Humidity > (humidityMean - standardFactor * humidityStandardDeviation))
-                            .Where(x => x.Humidity < humidityMean + standardFactor * humidityStandardDeviation)
-                            .Average(x => x.Humidity);
-                    }
-
-                    result.Humidity = filteredHumidity;
-
-                    _data = result;
-                    _series.Clear();
-                }
+                if (_sensor.IsLastReadSuccessful) _series.Add(result);
             }
         }
     }
@@ -97,13 +96,11 @@ namespace pidashboard.Services
 
     public class DhtWrapper
     {
-        private readonly DhtBase _dhtsensor;
-
         public DhtWrapper(DhtBase dhtsensor)
         {
-            _dhtsensor = dhtsensor;
+            DhtSensor = dhtsensor;
         }
 
-        public DhtBase DhtSensor => _dhtsensor;
+        public DhtBase DhtSensor { get; }
     }
 }
