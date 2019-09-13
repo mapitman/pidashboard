4102c9e313f900dbadb10f9db48be7eb7ae5e6f2
Mark Pitman <mark.pitman@gettyimages.com>
2019-09-13 09:49:34 -0700

    Filter out wildly different temp and humidity values
    Use a series of 10 values and throw out values that are outside the
    standard deviation by a factor of two

--- Services/EnvironmentalSensor.cs
+++ Services/EnvironmentalSensor.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using Iot.Device.DHTxx;
 using Iot.Units;
@@ -10,12 +12,12 @@ namespace pidashboard.Services
         private readonly DhtBase _sensor;
         private EnvironmentalSensorData _data;
         private readonly Timer _dataTimer;
+        private List<EnvironmentalSensorData> _series = new List<EnvironmentalSensorData>();
 
         public EnvironmentalSensor(DhtWrapper sensor)
         {
             _sensor = sensor.DhtSensor;
-            ReadData(null);
-            _dataTimer = new Timer(ReadData, null, TimeSpan.Zero, TimeSpan.FromSeconds(30));
+            _dataTimer = new Timer(ReadData, null, TimeSpan.Zero, TimeSpan.FromSeconds(1));
         }
 
         public EnvironmentalSensorData Data => _data;
@@ -33,16 +35,55 @@ namespace pidashboard.Services
             {
                 result.Temperature = _sensor.Temperature;
                 result.Humidity = _sensor.Humidity;
-                for (var i = 0; i < 10; i++)
-                {
-                    if (_sensor.IsLastReadSuccessful) break;
-                    Thread.Sleep(1000);
-                    result.Temperature = _sensor.Temperature;
-                    result.Humidity = _sensor.Humidity;
-                }
+
                 if (_sensor.IsLastReadSuccessful)
                 {
+                    _series.Add(result);
+                }
+
+                if (_series.Count >= 10)
+                {
+                    var standardFactor = 2.0;
+                    var temperatureMean = _series.Average(x => x.Temperature.Fahrenheit);
+                    var temperatureStandardDeviation = Math.Sqrt(_series.Select(x => Math.Pow((x.Temperature.Fahrenheit - temperatureMean), 2)).Sum()/_series.Count);
+
+                    double filteredTemperature;
+                    if (Math.Abs(temperatureStandardDeviation) < 1)
+                    {
+                        filteredTemperature = _series.First().Temperature.Fahrenheit;
+                    }
+                    else
+                    {
+                        filteredTemperature = _series.Where(x =>
+                                x.Temperature.Fahrenheit >
+                                (temperatureMean - standardFactor * temperatureStandardDeviation))
+                            .Where(x => x.Temperature.Fahrenheit <
+                                        temperatureMean + standardFactor * temperatureStandardDeviation)
+                            .Average(x => x.Temperature.Fahrenheit);
+                    }
+
+                    result.Temperature = Temperature.FromFahrenheit(filteredTemperature);
+                    
+                    var humidityMean = _series.Average(x => x.Humidity);
+                    var humidityStandardDeviation = Math.Sqrt(_series.Select(x => Math.Pow((x.Humidity - humidityMean), 2)).Sum()/_series.Count);
+
+                    double filteredHumidity;
+                    if (Math.Abs(humidityStandardDeviation) < 1)
+                    {
+                        filteredHumidity = _series.First().Humidity;
+                    }
+                    else
+                    {
+                        filteredHumidity = _series.Where(x =>
+                                x.Humidity > (humidityMean - standardFactor * humidityStandardDeviation))
+                            .Where(x => x.Humidity < humidityMean + standardFactor * humidityStandardDeviation)
+                            .Average(x => x.Humidity);
+                    }
+
+                    result.Humidity = filteredHumidity;
+
                     _data = result;
+                    _series.Clear();
                 }
             }
         }
